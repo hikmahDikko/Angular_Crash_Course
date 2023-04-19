116055fae92d50e5cd79fb905c155439587ff7ef
Hikmah Dikko <dhywis31@gmail.com>
2023-04-19 15:56:11 -0400

    initial commit

---
+++ hotel_inventory_app/src/app/rooms/rooms.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { Room, RoomList } from './rooms';
+
+@Component({
+  selector: 'app-rooms',
+  templateUrl: './rooms.component.html',
+  styleUrls: ['./rooms.component.scss']
+})
+export class RoomsComponent {
+  hotelName = 'Hikmah Hotel';
+  numberOfRooms = 120;
+  hideRooms = false;
+
+  rooms: Room = {
+    totalRooms: 120,
+    availableRooms : 40,
+    bookedRooms : 80
+  }
+
+  roomsList: RoomList[]=[{
+    roomNumber : 1,
+    roomType : 'Deluxe Room',
+    amenities : 'Air Conditional, Free Wi-Fi, Bluetooth',
+    price : 500,
+    image : "https://www.dreamstime.com/stock-images-hotel-room-image23802674",
+    checkInTime:new Date("10-Feb-2023"),
+    checkOutTime : new Date("12-Feb-2023"),
+  },
+  {
+    roomNumber : 2,
+    roomType : 'Deluxe Room',
+    amenities : 'Air Conditional, Free Wi-Fi, Bluetooth',
+    price : 500,
+    image : "https://www.dreamstime.com/stock-images-hotel-room-image23802674",
+    checkInTime:new Date("10-Feb-2023"),
+    checkOutTime : new Date("12-Feb-2023"),
+  },
+  {
+    roomNumber : 3,
+    roomType : 'Deluxe Room',
+    amenities : 'Air Conditional, Free Wi-Fi, Bluetooth',
+    price : 500,
+    image : "https://www.dreamstime.com/stock-images-hotel-room-image23802674",
+    checkInTime:new Date("10-Feb-2023"),
+    checkOutTime : new Date("12-Feb-2023"),
+  }]
+
+  toggle(){
+    this.hideRooms = !this.hideRooms;
+  }
+}
